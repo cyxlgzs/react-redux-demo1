dbce32c47a0a8735e0728e6b6d82bd195f5ebacb
jeff <jeff.lai@tradove.net>
2018-11-07 16:29:00 +0800

    test

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,7 @@ import openSocket from 'socket.io-client';
 
 
 function getNewSocket(){
-    return openSocket("ws://47.99.217.151:3000");
+    return openSocket("http://47.99.217.151:3000");
 }
 
 export default getNewSocket;
